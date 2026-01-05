cdeaa3f8337a98055fd6d54bcd36febad9c09354
Muhammad-Salman-khan <alibinkhan465@gmail.com>
2026-01-05 21:23:47 +0500

    feat: initialize project with TanStack Router and TanStack Query integration
    - Set up main entry point with React, TanStack Router, and TanStack Query.
    - Implement reportWebVitals for performance monitoring.
    - Generate route tree automatically with TanStack Router.
    - Create root route with context and integrate devtools for TanStack Router and Query.
    - Define index route with a simple component.
    - Add Tailwind CSS for styling and custom theme variables.
    - Configure TypeScript with strict settings and module resolution.
    - Set up Vite configuration with necessary plugins for React, TanStack Router, and Tailwind CSS.

---
+++ src/routes/__root.tsx
@@ -0,0 +1,36 @@
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+import Header from '../components/ui/Navbar/Header'
+
+import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
+
+import type { QueryClient } from '@tanstack/react-query'
+
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  component: () => (
+    <>
+      <div>
+        <Header />
+        <Outlet />
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+            TanStackQueryDevtools,
+          ]}
+        />
+      </div>
+    </>
+  ),
+})
