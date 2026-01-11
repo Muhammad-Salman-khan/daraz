df43de69e12f20864bdd7d7898a985fd82c959c9
Muhammad-Salman-khan <alibinkhan465@gmail.com>
2026-01-11 16:15:37 +0500

    feat: add HeadContent component and update meta tags for improved SEO

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,8 @@
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
+import {
+  HeadContent,
+  Outlet,
+  createRootRouteWithContext,
+} from '@tanstack/react-router';
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
 import { TanStackDevtools } from '@tanstack/react-devtools';
 
@@ -14,9 +18,30 @@ interface MyRouterContext {
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
+  head: () => ({
+    title: 'Daraz.pk Buy and sell from trusted sellers',
+    meta: [
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+
+      {
+        name: 'description',
+        content:
+          'Daraz is a ecomerce website build for buying and selling and customer',
+      },
+
+      {
+        name: 'robots',
+        content: 'index, follow',
+      },
+    ],
+  }),
   component: () => (
     <>
       <div className=" text-foreground  min-h-screen max-w-screen">
+        <HeadContent />
         <Header />
         <Outlet />
         <FooterSection />
